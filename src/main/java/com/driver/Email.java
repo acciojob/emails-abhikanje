a7547da2f0c791f5282e8a91aaf38f73d2cb52af
ABHISHEK KANAJE <102962455+abhikanje@users.noreply.github.com>
2023-02-18 18:44:39 +0530

    Update Email.java

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,28 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(!oldPassword.equals(this.password)){
+           return;
+        }
+        if(conditons(newPassword)){
+            this.password = newPassword;
+
+        }
+    }
+    private boolean conditons(String newPassword) {
+
+        if(newPassword.length()<8) return false;
+        boolean upper = false;
+        boolean lower = false;
+        boolean digit = false;
+        boolean special = false;
+
+        for(int i=0;i<newPassword.length();i++){
+            if(newPassword.charAt(i)>='A' && newPassword.charAt(i)<='Z') upper = true;
+            else if(newPassword.charAt(i)>='a' && newPassword.charAt(i)<='z') lower = true;
+            else if(newPassword.charAt(i)>='0' && newPassword.charAt(i)<='9') digit = true;
+            else special = true;
+        }
+        return upper && lower && digit && special;
     }
 }
